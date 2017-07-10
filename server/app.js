a460a35372774835a3321f2bad8f1e6940b1fe4e
evanyuanzh <evanyuanzh@evanyuanzhdeMacBook-Pro.local>
2017-07-10 15:22:55 +0800

    压缩图片

--- server/app.js
+++ server/app.js
@@ -7,19 +7,18 @@ const logger = require('koa-logger')
 
 const index = require('./routes/index')
 
-// error handler
 onerror(app)
 
-// middlewares
 app.use(bodyparser({
   enableTypes: ['json', 'form', 'text']
-}))
+}));
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public', {
   maxage: 3600000
 }));
 
+//允许跨域
 app.use(async function (ctx, next) {
   await next();
   ctx.set('Access-Control-Allow-Origin', '*');
