5347119db603ad5fb70679b9d7ccac44832fb578
evanyuanzh <evanyuanzh@evanyuanzhdeMacBook-Pro.local>
2017-07-12 10:52:44 +0800

    优化build压缩配置

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,11 +5,20 @@ const env = process.env.NODE_ENV.trim() // 当前环境
 const UglifyJsPlugin = new webpack
   .optimize
   .UglifyJsPlugin({
-
-    //压缩
+    // 最紧凑的输出
+    beautify: false,
+    // 删除所有的注释
+    comments: false,
     compress: {
-      // 在UglifyJs删除没有用到的代码时不输出警告
+      // 在UglifyJs删除没有用到的代码时不输出警告  
       warnings: false,
+      // 删除所有的 `console` 语句
+      // 还可以兼容ie浏览器
+      drop_console: true,
+      // 内嵌定义了但是只用到一次的变量
+      collapse_vars: true,
+      // 提取出出现多次但是没有定义成变量去引用的静态值
+      reduce_vars: true,
     }
   });
 
